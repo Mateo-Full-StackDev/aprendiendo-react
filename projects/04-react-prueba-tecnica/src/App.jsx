bd9d6977be5a202aece147b880985a06a0b78a0b
Junior <juniorrr123321@gmail.com>
2025-05-21 01:02:45 -0500

    fetching data

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,15 +1,23 @@
 import { useEffect, useState } from 'react'
+import '../style.css'
 export function App () {
   const [fact, setFact] = useState()
-
+  const [imageUrl, setImageUrl] = useState()
   const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(response => response.json())
       .then(data => {
-        setFact(data.fact)
-        console.log(data)
+        const { fact } = data
+        setFact(fact)
+        const threeFirstWord = fact.split(' ', 3).join(' ')
+        fetch(`https://cataas.com/cat/says/${threeFirstWord}?fontSize=40&json=true`)
+          .then(response => response.json())
+          .then(data => {
+            const { url } = data
+            setImageUrl(url)
+          })
       })
       .catch(error => console.log(error))
   }, [])
@@ -18,6 +26,7 @@ export function App () {
     <div>
       <h1>App de gatitos</h1>
       {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt="" />}
     </div>
   )
 }
